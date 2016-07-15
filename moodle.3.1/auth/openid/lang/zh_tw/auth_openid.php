f441cef90e7cd1be8e0769209d92678c13dd54f5
Ceasar Sun <ceasar@nchc.org.tw>
2016-07-15 14:50:53 +0800

    Delete moodle folder

--- moodle.3.1/auth/openid/lang/zh_tw/auth_openid.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$string['pluginname'] = 'OpenID';
-$string['auth_openiddescription'] = 'OpenID';
-$string['auth_login_button'] = '用 OpenID 登入';
-$string['auth_openid_error'] = '對不起，該縣市OpenID伺服器目前無法正常連線，請稍後再嘗試看看！';
-$string['auth_openid_tip'] = '請選擇您所在的縣市：';
-$string['auth_openidsettings'] = 'OpenID 認證設定';
-
