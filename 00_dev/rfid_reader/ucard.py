27f4e4853cb8e8545744b77333e33c84a3ccd64b
Thomas Tsai <thomas@nchc.org.tw>
2016-07-20 14:46:55 +0800

    update cardid to hex

--- 00_dev/rfid_reader/ucard.py
+++ 00_dev/rfid_reader/ucard.py
@@ -1,5 +1,5 @@
 import os
-import sqlite3
+#import sqlite3
 import os
 import time
 import datetime
@@ -11,9 +11,9 @@ from smartcard.util import toHexString
 debug=1
 
 # sqlite
-sqlitedb = "/home/thomas/work_house/ucard-reader/ucard_log"
-conn = sqlite3.connect(sqlitedb)
-cursor = conn.cursor()
+#sqlitedb = "/home/thomas/work_house/ucard-reader/ucard_log"
+#conn = sqlite3.connect(sqlitedb)
+#cursor = conn.cursor()
 
 # smart card reader
 #['Alcor Micro AU9522 00 00', 'Alcor Micro AU9522 00 01', 'NXP PR533 (3.70) 01 00']
@@ -22,13 +22,13 @@ if debug: print r
 connection = r[2].createConnection()
 
 # cache card id to local database
-def cache(cid):
-
-    log_datetime = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    cursor.execute("INSERT INTO log VALUES (?, ?, ?)", (log_datetime, cid, 'save'))
-    if debug: print [log_datetime, cid]
-    conn.commit()
-    return True
+#def cache(cid):
+#
+#    log_datetime = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+#    cursor.execute("INSERT INTO log VALUES (?, ?, ?)", (log_datetime, cid, 'save'))
+#    if debug: print [log_datetime, cid]
+#    conn.commit()
+#    return True
 
 def regist(cid):
     print "regist %s" % cid
@@ -49,7 +49,8 @@ while True:
 	if debug: print "%x %x" % (sw1, sw2)
 	#90 0
 	if debug: print data
-	cardid = ''.join(str(e) for e in data)
+	#cardid = ''.join(str(e) for e in data)
+	cardid = ''.join(hex(e).upper()[2:] for e in data)
 	if debug: print "cardid: %s" % cardid
 	#[201, 164, 245, 230]
 	time.sleep(1)
@@ -59,7 +60,7 @@ while True:
 	continue
 
 
-    cache(cardid)
+    #cache(cardid)
 #displaycard()
-    regist(cardid)
+    #regist(cardid)
 #finish()
