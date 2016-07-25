343df8deb0aa41a3f762415eca3f94be0fc08dc8
Thomas Tsai <thomas@nchc.org.tw>
2016-07-26 00:28:10 +0800

    test context

--- 00_dev/m.local/ucard/db/access.php
+++ 00_dev/m.local/ucard/db/access.php
@@ -17,21 +17,26 @@ $capabilities = array(
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes'   => array(
             'editingteacher' => CAP_ALLOW,
+	    'coursecreator' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'manager'  => CAP_ALLOW
         )
     ),
-
     'local/ucard:view' => array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes'   => array(
-            'user' => CAP_ALLOW,
-            'student' => CAP_ALLOW,
-            'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
+	    'coursecreator' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         )
-    )
-
+    ),
+    'local/ucard:viewlog' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes'   => array(
+	    'user'  =>CAP_ALLOW,
+        )
+    ),
 );
