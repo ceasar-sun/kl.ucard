e8c2b3c631e367854e353eb0a3a74850b8bd5187
Ceasar Sun <ceasar@nchc.org.tw>
2016-11-23 00:04:29 +0800

    deleted:    ../stat_book/.DS_Store

--- 00_dev/m.block/stat_book/runstat/bookrec_sql.R
+++
@@ -1,9 +0,0 @@
-library(RMySQL)
-library(htmlTable)
-
-png(file="Book.png", width=1500, height=800, res=120)
-book_account <- c(book0,book1,book2,book3,book4,book5,book6,book7,book8,book9)
-barplot(book_account)
-
-# Close Connection
-q()
